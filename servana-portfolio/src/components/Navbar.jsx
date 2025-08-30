1edee10919089d9bd8c24337773bbd171a52aacb
Kassim <kassimrooney@gmail.com>
2025-08-30 18:19:35 +0300

    Added smooth scroll in Navbar

--- servana-portfolio/src/components/Navbar.jsx
+++ servana-portfolio/src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-scroll";
 import { useState } from "react";
 import "../styles/Navbar.css";
 
@@ -8,11 +9,11 @@ function Navbar() {
     <nav className="navbar">
       <h1 className="logo">MyCompany</h1>
       <ul className={open ? "nav-links open" : "nav-links"}>
-        <li><a href="#hero">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#portfolio">Portfolio</a></li>
-        <li><a href="#contact">Contact</a></li>
+        <li><Link to="hero" smooth={true} duration={500}>Home</Link></li>
+        <li><Link to="about" smooth={true} duration={500}>About</Link></li>
+        <li><Link to="services" smooth={true} duration={500}>Services</Link></li>
+        <li><Link to="portfolio" smooth={true} duration={500}>Portfolio</Link></li>
+        <li><Link to="contact" smooth={true} duration={500}>Contact</Link></li>
       </ul>
       <button className="menu-toggle" onClick={() => setOpen(!open)}>
         ☰
