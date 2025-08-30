4273b0b856a4912e8138fa2b695e3cc34e688873
Kassim <kassimrooney@gmail.com>
2025-08-30 17:53:17 +0300

    Construct structure and installed dependancies

---
+++ servana-portfolio/src/App.jsx
@@ -0,0 +1,24 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import About from "./components/About";
+import Services from "./components/Services";
+import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import "./styles/App.css";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <About />
+      <Services />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
