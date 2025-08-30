c083028eb2c9c5e02e61f0e043243a4a3e259736
Kassim <kassimrooney@gmail.com>
2025-08-30 17:58:39 +0300

    Add Hero section and styled Navbar

---
+++ servana-portfolio/src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import "../styles/Hero.css";
+
+function Hero() {
+  return (
+    <section id="hero" className="hero">
+      <h2>Welcome to MyCompany</h2>
+      <p>We provide innovative business solutions to help you grow.</p>
+      <button>Get Started</button>
+    </section>
+  );
+}
+
+export default Hero;
