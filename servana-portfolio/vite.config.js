a6b65f6fda9a8cfd969d37146cbaf19084d94c5a
Kassim <kassimrooney@gmail.com>
2025-09-14 14:41:20 +0300

    Debug error

--- servana-portfolio/vite.config.js
+++ servana-portfolio/vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { imagetools } from 'vite-imagetools'
+import path from 'path'   // ✅ needed for alias paths
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +9,10 @@ export default defineConfig({
     react(),
     imagetools() // 👈 enable image optimization
   ],
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, 'node_modules/react'),
+      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
+    },
+  },
 })
